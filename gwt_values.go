8626250f63593599a2f14fa43c0c179b8404ce42
Jeremy Canady <jcanady@jackhenry.com>
2022-07-22 13:11:54 -0500

    Update GWT values

--- gwt_values.go
+++ gwt_values.go
@@ -10,20 +10,35 @@ const (
 
 	// GWTHeader is what appears to be a hash value that is provided at the beginning of every GWT request. As it
 	// changes with app updates it appears to be related to validating the version the requester is expecting.
-	GWTHeader = "3B6C5196158464C5643BA376AF05E7F1"
+	//GWTHeader = "3B6C5196158464C5643BA376AF05E7F1"
+	GWTHeader = "2D6A926E3729946302DC68073CB0D550"
 )
 
 // The following are the GWT procedure calls as found from inspection of the app.
 const (
+	//// GWTGenerateAuthToken will generate a GWT auth token. The only known use case is for accessing non GWT API calls
+	//// such as data export.
+	//// The first parameter in the string should be the sesnonce and the second is the users ID.
+	//GWTGenerateAuthToken = "7|0|8|https://cronometer.com/cronometer/|" + GWTHeader + "|com.cronometer.client.CronometerService|generateAuthorizationToken|java.lang.String/2004016611|I|com.cronometer.client.data.AuthScope/3337242207|%s|1|2|3|4|4|5|6|6|7|8|%s|3600|7|2|"
+	//
+	//// GWTAuthenticate will authenticate with the GWT api. The sesnonce should be set in the cookies.
+	//GWTAuthenticate = "7|0|5|https://cronometer.com/cronometer/|" + GWTHeader + "|com.cronometer.client.CronometerService|authenticate|java.lang.Integer/3438268394|1|2|3|4|1|5|5|-300|"
+	//
+	//// GWTLogout will log the session out.
+	//// The only parameter should be the sesnonce.
+	//GWTLogout = "7|0|6|https://cronometer.com/cronometer/|" + GWTHeader + "|com.cronometer.client.CronometerService|logout|java.lang.String/2004016611|%s|1|2|3|4|1|5|6|"
+
 	// GWTGenerateAuthToken will generate a GWT auth token. The only known use case is for accessing non GWT API calls
 	// such as data export.
 	// The first parameter in the string should be the sesnonce and the second is the users ID.
-	GWTGenerateAuthToken = "7|0|8|https://cronometer.com/cronometer/|" + GWTHeader + "|com.cronometer.client.CronometerService|generateAuthorizationToken|java.lang.String/2004016611|I|com.cronometer.client.data.AuthScope/3337242207|%s|1|2|3|4|4|5|6|6|7|8|%s|3600|7|2|"
+	GWTGenerateAuthToken = "7|0|8|https://cronometer.com/cronometer/|" + GWTHeader + "|com.cronometer.shared.rpc.CronometerService|generateAuthorizationToken" +
+		"|java.lang.String/2004016611|I|com.cronometer.shared.user.AuthScope/2065601159|%s|1|2|3|4|4|5|6|6|7|8|%s|3600|7|2|"
+	//"|java.lang.String/2004016611|I|com.cronometer.shared.rpc.data.AuthScope/3337242207|%s|1|2|3|4|4|5|6|6|7|8|%s|3600|7|2|"
 
 	// GWTAuthenticate will authenticate with the GWT api. The sesnonce should be set in the cookies.
-	GWTAuthenticate = "7|0|5|https://cronometer.com/cronometer/|" + GWTHeader + "|com.cronometer.client.CronometerService|authenticate|java.lang.Integer/3438268394|1|2|3|4|1|5|5|-300|"
+	GWTAuthenticate = "7|0|5|https://cronometer.com/cronometer/|" + GWTHeader + "|com.cronometer.shared.rpc.CronometerService|authenticate|java.lang.Integer/3438268394|1|2|3|4|1|5|5|-300|"
 
 	// GWTLogout will log the session out.
 	// The only parameter should be the sesnonce.
-	GWTLogout = "7|0|6|https://cronometer.com/cronometer/|" + GWTHeader + "|com.cronometer.client.CronometerService|logout|java.lang.String/2004016611|%s|1|2|3|4|1|5|6|"
+	GWTLogout = "7|0|6|https://cronometer.com/cronometer/|" + GWTHeader + "|com.cronometer.shared.rpc.CronometerService|logout|java.lang.String/2004016611|%s|1|2|3|4|1|5|6|"
 )
